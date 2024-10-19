3278e8d25f324be30d7ad3965ea6eadd944eb95b
M4to0429 <91228634+Mato0429@users.noreply.github.com>
2024-10-19 11:15:49 +0900

    Revert "[ファイル変更]"
    This reverts commit 9c89f0c69c078e7bdb2d1e97ccbbb1e02ecb880d.

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ class Config {}
 Config.stage_x = 10;
 Config.stage_y = 20;
 Config.stage_minoColor = '#3cb371';
-Config.stage_minoFallingDelay = 30; //フレーム
+Config.stage_minoFallingDelay = 120; //フレーム
 
 //ゲーム設定
-Config.game_frameDelay = 1000 / 30; //ms
-Config.game_rotateMinoDelay = 5; //フレーム
-Config.game_moveMinoDelay = 2; //フレーム
+Config.game_frameDelay = 1000 / 120; //ms
+Config.game_rotateMinoDelay = 20; //フレーム
+Config.game_moveMinoDelay = 8; //フレーム
